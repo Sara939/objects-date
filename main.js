87058097e8a0a2227ffaf65123b1e9d86743e4de
sara-likaount <saralikaunt@gmail.com>
2022-05-18 16:03:27 +0300

    b

---
+++ main.js
@@ -0,0 +1,38 @@
+// objects:
+
+// function myobject (it){
+// for(i=0; i<it.length; i++){
+//     console.log(it[i]);
+
+// }
+// }
+
+// myobject("i recive letters");
+
+// function secondfun(it){
+//     var result;
+//     for(i=0; i<it.length; i++){
+
+//        result+=it[i]+i;
+//     }
+//     console.log(result);
+//     return result;
+
+// }
+// secondfun("i recive letters");
+
+
+// function evendigits(any) {
+//     var neweven="";
+//     for (var i = 0; i < any.length; i++) {
+//         if (i % 2 == 0) {
+//             neweven+="2";
+//         }
+//         else{
+//             neweven+= any[i];
+//         }
+//     }
+//     console.log(neweven);
+// }
+
+// evendigits("my text string");
